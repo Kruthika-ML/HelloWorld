c4e2bb317d839f8563d9955d04633c918e3edfaf
Kruthika-ML <kruthikattimani@gmail.com>
2021-12-22 09:19:07 +0530

    commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,19 @@
-public class HelloWorld{
-public static void main(String[] args){
-System.out.println("Hello Universe");
-}
+public class IntEqualityPrinter {
+    public static void main(String[] args){
+        printEqual(1,1,1);
+        printEqual(1,2,1);
+        printEqual(-1,-1,-1);
+    }
+    
+    public static void printEqual(int a,int b,int c){
+        if(a<0 || b<0 || c<0){
+            System.out.println("Invalid value");
+        } else if (a==b==c){
+            System.out.println("all numbers are equal");
+        } else if (a!=b!=c){
+            System.out.println("All numbers are different");
+        } else {
+            System.out.println("Neither all are equal or different");
+        }
+    }
 }
